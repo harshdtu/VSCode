4fa65ef6d72d18a67228650bb95606480444e8f6
Harshvardhan Gupta <harshvardhan.dtu@gmail.com>
2016-06-19 18:18:25 +0530

    FirstBuild

---
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public enum WorkType
+    {
+        GoToMeetings, Golf, GenerateReports
+    }
+}
