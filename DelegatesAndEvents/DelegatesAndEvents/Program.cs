49864efb98cbcfc5af60b425a12ed65fd0be0d5b
Harshvardhan Gupta <harshvardhan.dtu@gmail.com>
2016-06-19 21:53:19 +0530

    Demo: Return value using a delegate - Finished

--- DelegatesAndEvents/DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/DelegatesAndEvents/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
-    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+    public delegate int WorkPerformedHandler(int hours, WorkType workType);
 
 
     class Program
@@ -15,8 +15,12 @@ namespace DelegatesAndEvents
         {
             WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
             WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+            WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
 
-            DoWork(del2);
+            del1 += del2 + del3;
+
+            int finalHours = del1(10, WorkType.GoToMeetings);
+            Console.WriteLine(finalHours);
             Console.Read();
 
         }
@@ -27,14 +31,22 @@ namespace DelegatesAndEvents
         }
         
 
-        static void WorkPerformed1(int hours, WorkType work)
+        static int WorkPerformed1(int hours, WorkType work)
         {
             Console.WriteLine("WorkPerformed1 Called " + hours.ToString());
+            return hours + 1;
         }
 
-        static void WorkPerformed2(int hours, WorkType work)
+        static int WorkPerformed2(int hours, WorkType work)
         {
             Console.WriteLine("WorkPerformed2 Called " + hours.ToString());
+            return hours + 2;
+        }
+
+        static int WorkPerformed3(int hours, WorkType work)
+        {
+            Console.WriteLine("WorkPerformed3 Called " + hours.ToString());
+            return hours + 3;
         }
     }
 
